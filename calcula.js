0713a22addb8d28aadb9ecc0b498e504546a5bbc
rebeca.pachecot@gmail.com <rebeca.pachecot@gmail.com>
2023-02-08 22:28:20 +0100

    calculadora

--- calcula.js
+++ calcula.js
@@ -4,7 +4,7 @@ const texto2 = document.getElementById("op2")
 const btnCalcular = document.getElementById("calcular")
 const pResultado = document.getElementById("resultado")
 
-//console.log(texto2)
+console.log(texto2)
 
 btnCalcular.addEventListener('click', calcular)
 
@@ -15,7 +15,7 @@ function calcular (){
     const op2 = parseFloat (texto2.value)
 
 
-    if((operador == "+" || operador == "-" || operador =="*" || 
+   if((operador == "+" || operador == "-" || operador =="*" || 
     operador == "/") && !isNaN(op1) && !isNaN(op2)){
         let resultado;
         switch(operador){
@@ -35,7 +35,55 @@ function calcular (){
         pResultado.style = "color:black"
         pResultado.innerText = "=" + resultado
         }else{
-            pResultado.style = "color:red"
-            pResultado.innerText = "calculo imposible"
+           pResultado.style = "color:red"
+           pResultado.innerText = "calculo imposible"
         }
     }
+
+
+
+
+
+
+const sumar = (num1, num2)=> {
+  return parseInt(num1) + parseInt(num2);
+}
+const restar = (num1, num2)=> {
+  return parseInt(num1) - parseInt(num2);
+}
+const dividir = (num1, num2)=> {
+  return parseInt(num1) / parseInt(num2);
+}
+const multiplicar = (num1, num2)=> {
+  return parseInt(num1) * parseInt(num2);
+}
+alert("¿que operacion deseas realizar?");
+let operacion = prompt("1: suma, 2: resta, 3: division, 4: multiplicacion");
+ 
+if (operacion == 1) {
+  let numero1 = prompt("primero numero para sumar");
+  let numero2 = prompt("segundo numero para sumar");
+  total = sumar(numero1,numero2);
+  alert(`tu total es ${total}`);
+ }
+ else if (operacion == 2) {
+  let numero1 = prompt("primero numero para restar");
+  let numero2 = prompt("segundo numero para restar");
+  total = restar(numero1,numero2);
+  alert(`tu total es ${total}`);
+ }
+ else if (operacion == 3) {
+  let numero1 = prompt("primero numero para multiplicar");
+  let numero2 = prompt("segundo numero para multiplicar");
+  total = multiplicar(numero1,numero2);
+  alert(`tu total es ${total}`);
+ }
+ else if (operacion == 4) {
+  let numero1 = prompt("primero numero para dividir");
+  let numero2 = prompt("segundo numero para dividir");
+  total = dividir(numero1,numero2);
+  alert(`tu total es ${total}`);
+ }
+ else {
+  alert("nose a encontrado la operacion")
+ }
